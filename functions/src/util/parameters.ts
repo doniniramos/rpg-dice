8007d31b6e7f818cc4e3f3d28ac0a1d7d1b683a5
franciscodonini <franciscodoniniramos@gmail.com>
2020-04-09 19:53:00 -0300

    # Nit {}

--- functions/src/util/parameters.ts
+++ functions/src/util/parameters.ts
@@ -1,25 +1,28 @@
 
 export async function checkMandatoryFieldsOnData(fields: any, data: any, functionName: any) {
 
-    if (data === undefined)
+    if (data === undefined) {
         return {
             statusCode: 500,
             message: 'Data is undefined on checkMandatoryFieldsOnData(' + functionName + ')'
         }
+    }
 
     for (const field of fields) {
 
-        if (data[field] === undefined)
+        if (data[field] === undefined) {
             return {
                 statusCode: 500,
                 message: 'The ' + field + ' is undefined!'
             }
+        }
 
-        if (data[field] === '')
+        if (data[field] === '') {
             return {
                 statusCode: 500,
                 message: 'The ' + field + ' is empty!'
             }
+        }
     }
 
     return null
